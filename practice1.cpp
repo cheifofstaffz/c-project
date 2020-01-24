fabaa703fad1f759b32609acfe2cce8614177fa2
cheifofstaffz <handulles@gmail.com>
2020-01-24 14:36:14 -0800

    first commit

---
+++ practice1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "common.h"
+using namespace std;
+
+int main(){
+
+
+int sum = add(5, 4);
+ cout<< "Hello  " << sum <<endl;
+
+
+
+}
---
+++ practice2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int add(int a, int b)
+
+{
+    int c;
+    c = a+b;
+    return c;
+}
